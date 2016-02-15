083537f87ae8e4dc4ac39e7cf882816a70368eea
cai <jack_czm@vip.sina.com>
2016-02-15 15:31:24 +0800

    fix

--- util/wechat_oauth.py
+++ util/wechat_oauth.py
@@ -83,7 +83,7 @@ def send_msg(receive_open_id, applicant_name, start_datetime, end_datetime, _typ
                    "msgtype": "text",
                    "agentid": %s,
                    "text": {
-                       "content": "%s, 您好。您的%s申请 %s 至 %s %s  天已通过，请点击申请记录进行查看"
+                       "content": "%s, 您好。您的%s申请 %s 至 %s 共 %s 天已通过，请点击申请记录进行查看"
                    },
                    "safe":"0"
                 }
@@ -95,7 +95,7 @@ def send_msg(receive_open_id, applicant_name, start_datetime, end_datetime, _typ
                    "msgtype": "text",
                    "agentid": %s,
                    "text": {
-                       "content": "您的部门同事%s申请%s至%s %s %s 天, 请您点击审批按钮进行批准。"
+                       "content": "您的部门同事%s申请%s至%s %s 共 %s 天, 请您点击审批按钮进行批准。"
                    },
                    "safe":"0"
                 }
@@ -107,12 +107,11 @@ def send_msg(receive_open_id, applicant_name, start_datetime, end_datetime, _typ
                    "msgtype": "text",
                    "agentid": %s,
                    "text": {
-                       "content": "%s, 您好。您的%s申请 %s 至 %s %s  天未通过, 请点击申请记录进行查看。"
+                       "content": "%s, 您好。您的%s申请 %s 至 %s 共 %s 天未通过, 请点击申请记录进行查看。"
                    },
                    "safe":"0"
                 }
                  """ % (receive_open_id, AGENT_ID, applicant_name, _type, start_datetime, end_datetime, days)
-    print(11, content.encode('utf-8'))
     requests.post(SEND_MSG_URL % access_token, data=content.encode('utf-8'))
 
 
